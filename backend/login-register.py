5bb8d08af90aeef5ab0827b4344d525a4fa7f1f3
ArshnoorMac <arandha@purdue.edu>
2024-02-17 18:18:53 -0500

    backend

--- backend/login-register.py
+++ backend/login-register.py
@@ -1,8 +1,7 @@
-from flask import Flask
+from website import create_app
 
-app = Flask(__name__)
+app = create_app
 
-@app.route('/')
 def hello():
     return 'Hello, World! This is your Flask backend.'
 
