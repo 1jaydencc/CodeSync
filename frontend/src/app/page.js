19a7102c8199e3991a852edbf3ad08674854ea1c
1jaydencc <cheung69@purdue.edu>
2024-02-29 19:08:33 -0500

    fixed router

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { auth } from '@/firebase-config.js';
 import { signInWithEmailAndPassword } from "firebase/auth";
 
