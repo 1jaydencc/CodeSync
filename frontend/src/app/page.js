35d357ce81f243410b6f73624967f04e922d819f
1jaydencc <cheung69@purdue.edu>
2024-03-28 19:16:51 -0400

    github login replaced email+pw

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,7 +4,11 @@
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { auth } from "@/firebase-config";
-import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import {
+  onAuthStateChanged,
+  signInWithPopup,
+  GithubAuthProvider,
+} from "firebase/auth";
 import { useEffect } from "react";
 
 import "@/app/login.css";
@@ -20,6 +24,24 @@ export default function Home() {
   const [showNotification, setShowNotification] = useState(false);
 
   const router = useRouter();
+
+  const handleGitHubLogin = async () => {
+    const provider = new GithubAuthProvider();
+    console.log("GitHub login");
+    try {
+      const result = await signInWithPopup(auth, provider);
+      // This gives you a GitHub Access Token.
+      const token = result.credential.accessToken;
+
+      // The signed-in user info.
+      const user = result.user;
+
+      console.log(user);
+      router.push("/editor"); // Redirect the user after successful login
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -37,98 +59,19 @@ export default function Home() {
     return () => unsubscribe();
   }, [router]);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const { email, password } = formData;
-
-    try {
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        email,
-        password,
-      );
-      if (!userCredential.user.emailVerified) {
-        setFormData({ email: "", password: "" });
-        setError("Please verify your email.");
-        setShowNotification(true);
-      } else {
-        router.push("/editor");
-      }
-      console.log(userCredential.user);
-    } catch (error) {
-      setFormData({ email: "", password: "" });
-      setError(getErrorMessage(error.code));
-      setShowNotification(true);
-    }
-  };
-
-  const getErrorMessage = (errorCode) => {
-    const errorMessages = {
-      "auth/invalid-credential": "No account found.",
-      "auth/too-many-requests": "Too many requests, try again in a bit.",
-    };
-
-    return errorMessages[errorCode] || errorCode;
-  };
-
   return (
     <div className="wrapper">
-      <form onSubmit={handleSubmit}>
-        <h1>Login</h1>
-        <div className="input-box">
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="input-box">
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-
-        <div className="forgot-password">
-          <a href="/password-reset">Forgot Password?</a>
-        </div>
-        <div className="divider">
-          <p>___________________________</p>
-        </div>
-        <div className="register-link">
-          <p>
-            <a href="/register">Create Account</a>
-          </p>
-        </div>
-      </form>
       <div>
-        {showNotification && (
-          <div className="notification-overlay">
-            <div className="notification">
-              <p>{error}</p>
-              <button onClick={() => setShowNotification(false)}>
-                Try Again
-              </button>
-            </div>
-          </div>
-        )}
+        <button onClick={handleGitHubLogin}>Login with GitHub</button>
       </div>
+      {showNotification && (
+        <div className="notification-overlay">
+          <div className="notification">
+            <p>{error}</p>
+            <button onClick={() => setShowNotification(false)}>Close</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
