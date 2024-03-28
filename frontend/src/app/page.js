30ce8d5745c92d2c39bac42dc56f0b19d30bba86
1jaydencc <cheung69@purdue.edu>
2024-03-27 21:23:40 -0400

    replaced tauri serve with electron

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,22 +1,22 @@
 // @/app/page
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { auth } from '@/firebase-config';
-import { signInWithEmailAndPassword, onAuthStateChanged} from "firebase/auth";
-import { useEffect } from 'react';
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import { auth } from "@/firebase-config";
+import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
 
-import '@/app/login.css';
-import '@/app/globals.css';
+import "@/app/login.css";
+import "@/app/globals.css";
 
 export default function Home() {
   const [formData, setFormData] = useState({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   });
 
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [showNotification, setShowNotification] = useState(false);
 
   const router = useRouter();
@@ -25,17 +25,17 @@ export default function Home() {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         // User is signed in, redirect them to the editor
-        // router.push('/editor');
+        console.log("User is signed in");
+        router.push("/editor");
       } else {
         // User is signed out, stay on this page or handle accordingly
-        console.log('User is not signed in');
+        console.log("User is not signed in");
       }
     });
 
     // Clean up the subscription on unmount
     return () => unsubscribe();
   }, [router]);
-  
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -50,18 +50,21 @@ export default function Home() {
     const { email, password } = formData;
 
     try {
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password,
+      );
       if (!userCredential.user.emailVerified) {
-        setFormData({ email: '', password: '' });
-        setError('Please verify your email.');
+        setFormData({ email: "", password: "" });
+        setError("Please verify your email.");
         setShowNotification(true);
-      }
-      else {
-        router.push('/editor');
+      } else {
+        router.push("/editor");
       }
       console.log(userCredential.user);
     } catch (error) {
-      setFormData({ email: '', password: '' });
+      setFormData({ email: "", password: "" });
       setError(getErrorMessage(error.code));
       setShowNotification(true);
     }
@@ -69,59 +72,63 @@ export default function Home() {
 
   const getErrorMessage = (errorCode) => {
     const errorMessages = {
-      'auth/invalid-credential': 'No account found.',
-      'auth/too-many-requests': 'Too many requests, try again in a bit.' 
+      "auth/invalid-credential": "No account found.",
+      "auth/too-many-requests": "Too many requests, try again in a bit.",
     };
 
     return errorMessages[errorCode] || errorCode;
   };
 
   return (
-    <div className='wrapper'>
-        <form onSubmit={handleSubmit}>
-            <h1>Login</h1>
-            <div className='input-box'>
-              <input
-                type='email'
-                name='email'
-                placeholder='Email'
-                value={formData.email}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className='input-box'>
-              <input
-                type='password'
-                name='password'
-                placeholder='Password'
-                value={formData.password}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <button type='submit'>Login</button>
-
-            <div className='forgot-password'>
-                <a href="/password-reset">Forgot Password?</a>
-            </div>
-            <div className='divider'>
-                <p>___________________________</p>
-            </div>
-            <div className='register-link'>
-                <p><a href="/register">Create Account</a></p>
-            </div>
-        </form>
-        <div>
-          {showNotification && (
-            <div className='notification-overlay'>
-              <div className='notification'>
-              <p>{error}</p>
-              <button onClick={() => setShowNotification(false)}>Try Again</button>
-              </div>
-            </div>
-          )}
+    <div className="wrapper">
+      <form onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <div className="input-box">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
         </div>
+        <div className="input-box">
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+
+        <div className="forgot-password">
+          <a href="/password-reset">Forgot Password?</a>
+        </div>
+        <div className="divider">
+          <p>___________________________</p>
+        </div>
+        <div className="register-link">
+          <p>
+            <a href="/register">Create Account</a>
+          </p>
+        </div>
+      </form>
+      <div>
+        {showNotification && (
+          <div className="notification-overlay">
+            <div className="notification">
+              <p>{error}</p>
+              <button onClick={() => setShowNotification(false)}>
+                Try Again
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
